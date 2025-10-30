1af2c4fb56e26d61425e39a8e78d8a79176200bd
Carlos App <107434698+CarlosEApp@users.noreply.github.com>
2025-10-30 13:12:18 -0300

    Add files via upload

--- JS/cad.js
+++ JS/cad.js
@@ -1,18 +1,41 @@
 
 
+//Limpar campos
+function limparC(){
 
+document.getElementById('ListaCategoria').value=''
+document.getElementById('imgcad').src;   
+document.getElementById('Input_ID').value=''
+document.getElementById('Input_titulo').value=''
+document.getElementById('Input_Subtitulo').value=''
+document.getElementById('Input_Valor').value=''
+document.getElementById('Input_ValorDesconto').value=''
+document.getElementById('Input_OBS').value=''
+document.getElementById('Input_add1').value=''
+document.getElementById('Input_add2').value=''
+document.getElementById('Input_add3').value=''
+document.getElementById('Input_add4').value=''
+document.getElementById('Input_add5').value=''
+}
 
 //salvar cadastro
 
 function SalvarCad(){
+var data= localStorage.getItem('data')
+var hora= localStorage.getItem('hora')
 var lista= document.getElementById('ListaCategoria').value;
 var imagem= document.getElementById('imgcad').src;   
-var idd= document.getElementById('Input_produto').value;
+var idd= document.getElementById('Input_ID').value;
 var titulo= document.getElementById('Input_titulo').value;
 var subt= document.getElementById('Input_Subtitulo').value;
 var valor=document.getElementById('Input_Valor').value;
 var descontoV=document.getElementById('Input_ValorDesconto').value;
 var obs= document.getElementById('Input_OBS').value;
+var add1= document.getElementById('Input_add1').value;
+var add2= document.getElementById('Input_add2').value;
+var add3= document.getElementById('Input_add3').value;
+var add4= document.getElementById('Input_add4').value;
+var add5= document.getElementById('Input_add5').value;
 
 if(!lista||lista==''||!idd||idd==''||!titulo|| titulo==''||!valor||valor==''){
     Swal.fire({
@@ -52,9 +75,19 @@ db.collection(`${lista}`).doc(`${idd}`).set({
     Lista:lista,
     OBS:obs,
     Imagem:imagem,
+    ADD1:add1,
+    ADD2:add2,
+    ADD3:add3,
+    ADD4:add4,
+    ADD5:add5,
+    Data:data,
+    Hora:hora,
+
 })
 setTimeout(function(){
+     FecharCad()
  Swal.fire('Sucesso!',`Salvo com o ID: ${idd} - Lista: ${lista}`,'success')
+   limparC()
 },2000)
 
 }
@@ -66,7 +99,7 @@ setTimeout(function(){
 
 //Storage
 function imagem(){
-    var idd= document.getElementById('Input_produto').value;
+var idd= document.getElementById('Input_ID').value;
 document.getElementById('files').click();
 var firebaseConfigure = {
 apiKey: "AIzaSyBCvQECt03lGjQv6rMCPnP19uI8inxgKxQ",
@@ -142,7 +175,7 @@ let codigo = '';
 for (let i = 0; i < 8; i++) {
   codigo += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
 }
-document.getElementById('Input_produto').value= codigo+tres
+document.getElementById('Input_ID').value= codigo+tres
 
 }
 // botão fechar cadastro
