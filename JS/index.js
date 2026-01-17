81eefa1f12dbeedbdb43d251c95a82b364a2e94a
Carlos App <107434698+CarlosEApp@users.noreply.github.com>
2026-01-17 00:45:04 -0300

    Add files via upload

--- JS/index.js
+++ JS/index.js
@@ -514,10 +514,10 @@ window.open('html/cad.html', '_self');
 //fale conosco
 var telefone= sessionStorage.getItem('teladmin')
 function falecom(){
-var url = encodeURIComponent("https://rd-reparos-domicilio.netlify.app");
+var url = encodeURIComponent("https://rd-reparos-domicilio.netlify.app/");
 var numero = `+55${telefone}`; // Substitua pelo número de destino, incluindo o código do país
 var msm=` R.D - Reparos a Domicílio (pedido de contato) \n\n`
-var Url = "https://wa.me/"+`${numero}?text=${encodeURIComponent(msm)} Página web: ${url} `;
+var Url = "https://wa.me/"+`${numero}?text=${encodeURIComponent(msm)} Página web: ${url}`;
 window.open(Url,"_blank" );
 }
 function ZAP(){
